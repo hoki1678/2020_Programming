d4335e50b3ec5c3f48faf55e109087322de0ff12
hoki1678 <63887921+hoki1678@users.noreply.github.com>
2020-04-25 12:35:52 +0900

    Add files via upload

---
+++ 0425/turtle02.py
@@ -0,0 +1,22 @@
+import turtle as t
+
+t.reset()
+
+t.shape("turtle")
+
+t.pensize(10)
+
+t.color("green")
+
+
+t.circle(100)
+
+t.penup()
+
+t.goto(200,0)
+
+t.pendown()
+
+t.color("black")
+
+t.circle(100)

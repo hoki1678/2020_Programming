a4228765147805ac7a390d0ec13f3516477b75a2
hoki1678 <63887921+hoki1678@users.noreply.github.com>
2020-05-23 12:58:44 +0900

    Add files via upload

---
+++ 0523/selc02.py
@@ -0,0 +1,8 @@
+num = int(input("정수 입력 : "))
+
+if num > 0:
+    print("양수")
+if num < 0:
+    pritn("음수")
+if num == 0:
+    print("0")
---
+++ 0523/selc05.py
@@ -0,0 +1,11 @@
+num = int(input("정수 입력 : "))
+
+if num == 0:
+    print("0")
+
+else:
+    if num > 0:
+        print("양수입니다.")
+    else:
+        print("음수입니다.")
+    
